141d4fbdfc1b5a6ccd0af5fbff72c3a1fe5077a7
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-19 07:06:02 -0600

    basic logic, will change styling next

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
+const resetButton = document.querySelector("#reset");
+
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
+const winningScoreSelect = document.querySelector("#playto");
+
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
@@ -13,6 +17,8 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -22,7 +28,32 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
 });
+
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  // reset the game if someone changes the winning score mid-game
+  reset();
+});
+
+// resetting game...
+
+resetButton.addEventListener("click", reset);
+
+function reset() {
+  // reset game
+  isGameOver = false;
+  // update scores to 0
+  p1Score = 0;
+  p2Score = 0;
+  // need to update display back to 0
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+}
